cb0ab11db113fdf77119f699cab40f68bfb89f98
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-10-06 19:13:19 +0300

    modified 37.js

---
+++ 37.js
@@ -0,0 +1,21 @@
+/* Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array. */
+function countPositivesSumNegatives(input) {
+    // your code here
+    if (input == null || input.length == 0)
+        return [];
+    let arr = [0, 0];
+    for (let i = 0; i < input.length; i++) {
+
+        if (input[i] > 0) {
+            arr[0] = arr[0] + 1;
+        } else if (input[i] < 0) {
+            arr[1] = arr[1] + input[i];
+        }
+    }
+
+    return arr;
+}
