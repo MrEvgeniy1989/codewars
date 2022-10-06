25becbb7bf3e4691f5c07a528e9eca336f59cd60
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-10-06 18:10:29 +0300

    modified 34.js

---
+++ 34.js
@@ -0,0 +1,8 @@
+/* Given an array of integers, return a new array with each value doubled. */
+function maps(x) {
+    let newX = [];
+    for (let i = 0; i < x.length; i++) {
+        newX[i] = x[i] * 2;
+    }
+    return newX;
+}
