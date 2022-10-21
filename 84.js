43e4a15f7be02386cdf410850d48b7774bfcdd96
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-10-21 13:39:04 +0300

    Create 84.js

---
+++ 84.js
@@ -0,0 +1,17 @@
+/* Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck! */
+function doubleChar(str) {
+    let newstr = "";
+    //   for (let i=0; i<str.length; i++) {
+    //     newstr += (str[i] + str[i]);
+    //   }
+    for (let char of str) {
+        newstr += (char + char);
+    }
+    return newstr;
+}
