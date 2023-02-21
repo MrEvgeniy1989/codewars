94e92049a9372c4fecb6d8eec0c1343e95f340d9
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-02-21 13:11:29 +0300

    Create 262.js

---
+++ 262.js
@@ -0,0 +1,20 @@
+/*
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.*/
+function humanReadable(seconds) {
+    let HH = Math.floor(seconds / 3600)
+    let MM = Math.floor((seconds % 3600) / 60)
+    let SS = (seconds % 3600) % 60
+
+    HH = HH >= 0 && HH < 10 ? '0' + HH : HH
+    MM = MM >= 0 && MM < 10 ? '0' + MM : MM
+    SS = SS >= 0 && SS < 10 ? '0' + SS : SS
+
+    return `${HH}:${MM}:${SS}`
+}
