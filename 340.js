186a358a13c3b38e8efc21edd504c4b190966155
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-05-10 17:21:46 +0300

    340

---
+++ 340.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5592e3bd57b64d00f3000047/train/javascript
+function findNb(m) {
+    let sum = 0;
+    let n = 1;
+    while (sum < m) {
+        sum += Math.pow(n, 3);
+        if (sum === m) {
+            return n;
+        }
+        n++;
+    }
+    return -1;
+}
