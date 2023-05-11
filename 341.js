a4f46f31c35b9fa75585a47aa304460527b25b75
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-05-11 16:01:29 +0300

    341

---
+++ 341.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5262119038c0985a5b00029f/
+function isPrime(num) {
+    if (num < 2) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+isPrime(73)
+isPrime(75)
