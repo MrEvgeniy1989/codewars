bc0c20403c41e4230250e7a79b772f6d716645c6
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-05-23 13:07:42 +0300

    353

---
+++ 353.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/559590633066759614000063/train/javascript
+function minMax(arr) {
+    return [Math.min(...arr), Math.max(...arr)]
+}
+
+minMax([1, 2, 3, 4, 5]); // [1,5]
