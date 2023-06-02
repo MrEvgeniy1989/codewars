0bb00d9eae3cb37f1bacac8881977ef4fdaf97d9
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-06-02 06:29:32 +0300

    363

---
+++ 363.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/582e4c3406e37fcc770001ad/train/javascript
+function joinStrings(string1, string2) {
+    return `${string1} ${string2}`
+}
+
+joinStrings('string1', 'string2')
+joinStrings('testing', 'testing')
