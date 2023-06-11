cf560be296ac555bf63ec05f319b858c820cc2de
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-06-11 10:44:38 +0300

    373

---
+++ 373.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/59811fd8a070625d4c000013/train/javascript
+function integrate(coefficient, exponent) {
+    return `${coefficient / (exponent + 1)}x^${exponent + 1}`
+}
+
+integrate(12, 5)
