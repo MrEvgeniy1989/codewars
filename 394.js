e937515374f71ec55c8077a335588f44d850d249
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-07-01 11:06:03 +0300

    394

---
+++ 394.js
@@ -0,0 +1,17 @@
+const rooms = {
+    "room1": {
+        "name": "Starting Room",
+        "description": "You find yourself in a dimly lit room with one door.",
+        "completed": false
+    },
+    "room2": {
+        "name": "Library",
+        "description": "The room is filled with dusty bookshelves and a large oak desk in the center.",
+        "completed": false
+    },
+    "room3": {
+        "name": "Laboratory",
+        "description": "Beakers and test tubes line the shelves of this room, and a strange smell fills the air.",
+        "completed": false
+    }
+};
