001d8fbe0d7744ad32a87b0a292d2984e584b0ae
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-07-21 22:47:59 +0300

    414

--- 414.js
+++ 414.js
@@ -1,4 +1,7 @@
 // https://www.codewars.com/kata/56c24c58e0c0f741d4001aef/train/javascript
 const isReallyNaN = (val) => {
-    return isNaN(val) && val !== undefined;
+    return typeof val === 'number' && isNaN(val);
 };
+
+
+isReallyNaN(NaN)
