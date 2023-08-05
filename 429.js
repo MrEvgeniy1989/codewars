1f506bf8515d16970a63aeca2576a7d2901491db
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-08-05 22:08:24 +0300

    429

---
+++ 429.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/563c13853b07a8f17c000022/train/javascript
+function isToday(date) {
+    const today = new Date();
+    return date.getDate() === today.getDate() &&
+        date.getMonth() === today.getMonth() &&
+        date.getFullYear() === today.getFullYear();
+}
