ba4634adb6fd149a105d2aa48d5d5d25e6a546df
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-08-27 21:58:38 +0300

    451

---
+++ 451.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/566dc05f855b36a031000048/train/javascript
+function addExtra(listOfNumbers) {
+    const newList = listOfNumbers.slice();
+
+    newList.push(13);
+
+    return newList;
+}
