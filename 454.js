492e7976190791dccc8e62e15edac62c3a6134eb
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-09-01 18:32:15 +0300

    454

--- 454.js
+++
@@ -1,6 +0,0 @@
-// https://www.codewars.com/kata/596e4ef7b61e25981200009f/train/javascript
-function aspectRatio(x, y) {
-    const targetAspectRatio = 16 / 9;
-    const newWidth = Math.ceil(y * targetAspectRatio);
-    return [newWidth, y];
-}
