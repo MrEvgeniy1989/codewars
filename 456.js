49f2ad099e17028f2a1f92b737867320dfca6703
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-09-01 18:38:48 +0300

    456

---
+++ 456.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/52a47dd7e950edabfa000391/train/javascript
+function getMax1() {
+    return {name: 'Max Headroom'}
+}
+
+function getMax2() {
+    return {name: 'Max Headroom'}
+}
