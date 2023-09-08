0a1a21ee644d2052a3285ef1a1aea0788d25c9f9
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-09-08 17:24:33 +0300

    463

---
+++ 463.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/514aa0dc21607ae236000017/train/javascript
+function greet_abe() {
+    var name = 'Abe';
+    return "Hello, " + name + '!';
+};
+
+function greet_ben() {
+    name = 'Ben';
+    return "Hello, " + name + '!';
+};
