b2442b380ed089eec0614d7a49a9293637e1651f
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-09-14 18:58:25 +0300

    468

---
+++ 468.js
@@ -0,0 +1,20 @@
+//https://www.codewars.com/kata/57238766214e4b04b8000011/train/javascript
+function changeMe(moneyIn) {
+    switch (moneyIn) {
+        case '£5':
+            var change = Array(25).fill('20p');
+            return change.join(' ');
+        case '£2':
+            var change = Array(10).fill('20p');
+            return change.join(' ');
+        case '£1':
+            var change = Array(5).fill('20p');
+            return change.join(' ');
+        case '50p':
+            return '20p 20p 10p';
+        case '20p':
+            return '10p 10p';
+        default:
+            return moneyIn;
+    }
+}
