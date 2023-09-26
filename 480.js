98d4b2f04dc209d0d4f90e31281f5a68db739f29
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-09-26 17:49:18 +0300

    480

--- 480.js
+++ 480.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 // https://www.codewars.com/kata/563f037412e5ada593000114/train/javascript
-=======
->>>>>>> origin/main
 function calculateYears(principal, interest, tax, desired) {
     return Math.ceil(
         Math.log(desired / principal) /
