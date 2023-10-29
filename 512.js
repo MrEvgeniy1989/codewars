548d1d140fd5b0f4a5a7dce841c793b19009f86b
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-10-29 15:22:35 +0300

    512

--- 512.js
+++ 512.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/5fde1ea66ba4060008ea5bd9/solutions
+// https://www.codewars.com/kata/5fde1ea66ba4060008ea5bd9/train/javascript
 function burner(c, h, o) {
     const water = Math.min(~~(h / 2), o)
     const carbon = Math.min(~~((o - water) / 2), c)
