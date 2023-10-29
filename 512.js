4fec941f7d9481d0a2ddebd5577f19fd87981ee7
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-10-29 15:22:00 +0300

    512

---
+++ 512.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5fde1ea66ba4060008ea5bd9/solutions
+function burner(c, h, o) {
+    const water = Math.min(~~(h / 2), o)
+    const carbon = Math.min(~~((o - water) / 2), c)
+    const methane = Math.min(~~((h - water * 2) / 4), c - carbon)
+    return [water, carbon, methane]
+}
