44fef96b08219cf86e53dd9c86fd09c8795df86b
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-11-29 21:42:52 +0300

    542

--- 542.js
+++ 542.js
@@ -3,7 +3,7 @@ function nbDig(n, d) {
     let res = 0;
     for (let i = 0; i <= n; i++) {
         let square = (i * i + "").split("");
-        square.forEach((s) => s === d ? res++ : null)
+        square.forEach((s) => s == d ? res++ : null)
     }
     return res;
 }
