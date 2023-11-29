117e4746bb4d25a2bea092fb3c06aa37c07996ea
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-11-29 21:40:57 +0300

    542

---
+++ 542.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/566fc12495810954b1000030/train/javascript
+function nbDig(n, d) {
+    let res = 0;
+    for (let i = 0; i <= n; i++) {
+        let square = (i * i + "").split("");
+        square.forEach((s) => s === d ? res++ : null)
+    }
+    return res;
+}
