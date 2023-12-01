f077d2901009f196e633fbd89e03c1a68a41e6cc
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-12-01 12:16:23 +0300

    544

---
+++ 544.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/545a4c5a61aa4c6916000755/train/javascript
+function gimme(triplet) {
+    const max = Math.max(...triplet)
+    const min = Math.min(...triplet)
+    for (let i = 0; i < triplet.length; i++) {
+        if (triplet[i] !== max && triplet[i] !== min) {
+            return i
+        }
+    }
+}
