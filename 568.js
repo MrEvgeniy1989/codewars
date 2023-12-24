6f43c7daff0509c52a031987943130b908206e09
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-12-24 07:54:21 +0300

    568

---
+++ 568.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57a049e253ba33ac5e000212/train/javascript
+function factorial(n) {
+    let result = 1
+    for (let i = 1; i <= n; i++) {
+        result *= i
+    }
+    return result
+}
