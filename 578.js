e071ed9e917908bbf99e2d4915942ae411d4a007
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-01-02 16:29:54 +0300

    578

---
+++ 578.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/javascript
+function twoSum(numbers, target) {
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+}
+
+twoSum([1, 2, 3], 4);
+twoSum([1234, 5678, 9012], 14690);
+twoSum([2, 2, 3], 4);
+twoSum([2, 3, 1], 3);
