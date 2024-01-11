c523f1ded38bac2ab06815b87372271228288dfc
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-01-11 15:52:00 +0300

    588

---
+++ 588.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/55c04b4cc56a697bb0000048/train/javascript
+function scramble(str1, str2) {
+    const charCount = {};
+
+    for (let char of str1) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    for (let char of str2) {
+        if (!charCount[char]) {
+            return false;
+        }
+        charCount[char]--;
+    }
+
+    return true;
+}
+
+scramble('rkqodlw', 'world');
