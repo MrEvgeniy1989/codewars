defbd5e3fa21387ccdff930a1524895aa514490a
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-01-28 11:48:52 +0300

    605

--- 605.js
+++ 605.js
@@ -1,15 +1,14 @@
 // https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/train/javascript
 multiplicationTable = function (size) {
-  let result = [[]]
+  let result = []
+
   for (let i = 0; i < size; i++) {
-    result[0].push(i + 1)
-  }
-  for (let i = 1; i < size; i++) {
     result[i] = []
     for (let j = 0; j < size; j++) {
-      result[i].push(result[0][j] * (i + 1))
+      result[i][j] = (i + 1) * (j + 1)
     }
   }
+
   return result
 }
 
