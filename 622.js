52f10b90a9e68f011a998e2afce9fa8502ff9045
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-02-13 16:22:05 +0300

    622

---
+++ 622.js
@@ -0,0 +1,65 @@
+class RomanNumerals {
+  static toRoman(num) {
+    const romanNumerals = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+    }
+
+    let result = ""
+
+    for (let key in romanNumerals) {
+      while (num >= romanNumerals[key]) {
+        result += key
+        num -= romanNumerals[key]
+      }
+    }
+
+    return result
+  }
+
+  static fromRoman(str) {
+    const romanNumerals = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+    }
+
+    let result = 0
+
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i]
+      let currentValue = romanNumerals[currentSymbol]
+      let nextValue = romanNumerals[str[i + 1]]
+
+      if (nextValue > currentValue) {
+        result += nextValue - currentValue
+        i++
+      } else {
+        result += currentValue
+      }
+    }
+
+    return result
+  }
+}
