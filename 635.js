37f194ba250c2215e25ba79fced0cfc0558730e5
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-02-26 15:31:27 +0300

    635

---
+++ 635.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/535474308bb336c9980006f2/train/javascript
+let greet = function (name) {
+  return `Hello ${name[0].toUpperCase() + name.substring(1).toLowerCase()}!`
+}
