cdf77a6eaa41f2571fad5c3a3b0e1cbd7314f0aa
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-03-13 21:21:16 +0300

    651

---
+++ 651.js
@@ -0,0 +1,15 @@
+function isRubyComing(list) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].language === "Ruby") {
+      return true
+    }
+  }
+
+  return false
+}
+
+isRubyComing([
+  { firstName: "Sofia", lastName: "I.", country: "Argentina", continent: "Americas", age: 35, language: "Java" },
+  { firstName: "Lukas", lastName: "X.", country: "Croatia", continent: "Europe", age: 35, language: "Python" },
+  { firstName: "Madison", lastName: "U.", country: "United States", continent: "Americas", age: 32, language: "Ruby" },
+])
