b879e469f4409212c7bd798995ddcab13d65febc
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-04-09 11:07:32 +0300

    678

---
+++ 678.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5a58d889880385c2f40000aa/train/javascript
+function automorphic(n) {
+  const str = Math.pow(n, 2).toString()
+  let result = str.substring(str.length - n.toString().length)
+  return Number(result) === n ? "Automorphic" : "Not!!"
+}
+
+automorphic(6)
