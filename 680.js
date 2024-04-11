2dc8f07f2882c87d867297c632545c7f5d7c1d97
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-04-11 10:20:07 +0300

    680

---
+++ 680.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5d50e3914861a500121e1958/train/javascript
+function addLetters(...letters) {
+  return String.fromCharCode(((letters.reduce((a, b) => a + b.charCodeAt(0) - 96, 0) + 25) % 26) + 97)
+}
+
+addLetters(["a", "b", "c"])
