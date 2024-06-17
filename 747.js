065f094981b611213073ea2124a1625f8a79aafb
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-06-17 23:00:46 +0300

    747

---
+++ 747.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/57c1ab3949324c321600013f/train/javascript
+const toLeetSpeak = (s) => s.replace(/[ABCEGHILOSTZ]/g, (c) => D[c])
+
+const D = {
+  A: "@",
+  B: "8",
+  C: "(",
+  E: "3",
+  G: "6",
+  H: "#",
+  I: "!",
+  L: "1",
+  O: "0",
+  S: "$",
+  T: "7",
+  Z: "2",
+}
