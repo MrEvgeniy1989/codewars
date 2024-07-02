2bcf551ae91ab47b87634788b0bad933ffc2f5d7
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-07-02 18:01:23 +0300

    762

---
+++ 762.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/54592a5052756d5c5d0009c3/train/javascript
+function head(a) {
+  return a[0]
+}
+
+function last(a) {
+  return a[a.length - 1]
+}
+
+function init(a) {
+  return a.slice(0, -1)
+}
+
+function tail(a) {
+  return a.slice(1)
+}
