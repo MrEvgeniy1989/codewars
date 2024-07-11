c9bd2e14a31c90fca81262dcb6d8c53195fa7c43
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-07-11 13:54:33 +0300

    771

---
+++ 771.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/57e8fba2f11c647abc000944/train/javascript
+function fireFight(s) {
+  return s.replace(/Fire/gi, "~~")
+}
+
+fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast")
