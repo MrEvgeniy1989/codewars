24ca74f4b1f14fda4c3bd3dfeaf00871af546327
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-09-08 20:03:52 +0300

    829

---
+++ 829.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/559cc2d2b802a5c94700000c/train/javascript
+function consecutive(arr) {
+  let l = arr.length
+  return l ? Math.max(...arr) - Math.min(...arr) - l + 1 : 0
+}
