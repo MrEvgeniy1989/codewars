635a764895d035eaa767d93edb1dd16c6b273798
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-10-31 12:57:46 +0300

    874

---
+++ 874.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5a4ff3c5fd56cbaf9800003e/train/javascript
+function withoutMax(arr) {
+  // Fix it
+  // arr.pop(); // removes the last element
+  return arr.slice(0, -1)
+}
