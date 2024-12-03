99291d74104658ecb83f9e5d9c98c570613151fb
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-12-03 14:19:24 +0300

    902

---
+++ 902.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5983cba828b2f1fd55000114/train/javascript
+function oddOne(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      return i
+    }
+  }
+  return -1
+}
