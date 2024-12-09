e6f0bcc5915efb28852680bc19857a6021e3b513
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-12-09 15:05:55 +0300

    908

---
+++ 908.js
@@ -0,0 +1,11 @@
+function battle(x, y) {
+  a = x
+    .split("")
+    .map((z) => z.charCodeAt(0) - 64)
+    .reduce((a, b) => a + b, 0)
+  b = y
+    .split("")
+    .map((z) => z.charCodeAt(0) - 64)
+    .reduce((a, b) => a + b, 0)
+  return a < b ? y : a > b ? x : "Tie!"
+}
