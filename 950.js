c29ff8d15c2d081ee1556b7ec362d54eaba7fc0c
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-01-22 09:23:58 +0300

    950

---
+++ 950.js
@@ -0,0 +1,17 @@
+function caffeineBuzz(n) {
+  let result = ""
+
+  if (n % 3 === 0 && n % 4 === 0) {
+    result = "Coffee"
+    if (n % 2 === 0) {
+      result += "Script"
+    }
+  } else if (n % 3 === 0) {
+    result = "Java"
+    if (n % 2 === 0) {
+      result += "Script"
+    }
+  }
+
+  return result || "mocha_missing!"
+}
