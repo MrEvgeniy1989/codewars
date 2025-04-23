ff517ab2f66d2c983a8f3279dce892afd8552110
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-04-23 22:56:57 +0300

    1006

---
+++ 1006.js
@@ -0,0 +1,10 @@
+function capitalsFirst(str) {
+  let words = str.split(" ")
+  let upper = words.filter(function (x) {
+    return x.charAt(0).match(/[A-Z]/)
+  })
+  let lower = words.filter(function (x) {
+    return x.charAt(0).match(/[a-z]/)
+  })
+  return upper.concat(lower).join(" ")
+}
