57d9252d3eebf9e91ba1cc273a4a9dbc3d33460b
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-05-09 19:01:40 +0300

    1020

---
+++ 1020.js
@@ -0,0 +1,11 @@
+const MATCH = {
+  scissors: ["paper", "lizard"],
+  paper: ["rock", "spock"],
+  rock: ["lizard", "scissors"],
+  lizard: ["spock", "paper"],
+  spock: ["scissors", "rock"],
+}
+
+function rpsls(pl1, pl2) {
+  return MATCH[pl1].indexOf(pl2) >= 0 ? "Player 1 Won!" : MATCH[pl2].indexOf(pl1) >= 0 ? "Player 2 Won!" : "Draw!"
+}
