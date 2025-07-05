96acc16ec31d3557558f1e8434898ba14bade28a
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-07-05 09:39:33 +0300

    1060

---
+++ 1060.js
@@ -0,0 +1,10 @@
+function pattern(n) {
+  var number = "",
+    output = ""
+  while (n > 0) {
+    number = n + number
+    output = number + (output ? "\n" : "") + output
+    n--
+  }
+  return output
+}
