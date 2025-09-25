1c4b8033e83e94e05d5a645fd8de52b2c6ef60b1
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-09-25 17:42:25 +0300

    1119

---
+++ 1119.js
@@ -0,0 +1,10 @@
+function solve(a, b) {
+  let [x, y] = [0, 0]
+  for (let i = 0; i < 3; i++) {
+    if (a[i] > b[i]) x++
+    else if (a[i] < b[i]) y++
+  }
+  if (x > y) return `${x}, ${y}: Alice made "Kurt" proud!`
+  else if (x < y) return `${x}, ${y}: Bob made "Jeff" proud!`
+  else return `${x}, ${y}: that looks like a "draw"! Rock on!`
+}
