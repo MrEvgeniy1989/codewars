eb4c27409eb93e5330dc2c0a642493c1ffdde135
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-10-02 11:45:04 +0300

    1125

---
+++ 1125.js
@@ -0,0 +1,8 @@
+function to24hourtime(hour, minute, period) {
+  if (period === "am" && hour === 12) {
+    hour = 0
+  } else if (period === "pm" && hour !== 12) {
+    hour += 12
+  }
+  return hour.toString().padStart(2, "0") + minute.toString().padStart(2, "0")
+}
