a2e67578b54e4a1209fb7fe54fe82a2b37662d3b
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2025-10-05 17:59:47 +0300

    1126

--- 1126.js
+++ 1126.js
@@ -1,3 +1,3 @@
-function processArray(arr, callback) {
-  return arr.map(callback);
+function uglifyWord(s) {
+  return s.replace(/[a-z]+/gi, (x) => x.split``.map((y, i) => (i % 2 ? y.toLowerCase() : y.toUpperCase())).join``)
 }
